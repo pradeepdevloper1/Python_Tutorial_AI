b7a45d64034f6ec80259022b15eed7a9d2fb655a
Pradeep Kumar <67544291+pradeepdevloper1@users.noreply.github.com>
2020-08-01 14:50:42 +0530

    Update 41functions.py
    return in function

--- 41functions.py
+++ 41functions.py
@@ -9,3 +9,19 @@ print_name('Pradeep')
 #Doc String 
 print("Doc string "+print_name.__doc__)
  
+    
+    
+def get_sum(lst):
+    """
+    Return sum of list 
+    """
+    _sum=0
+    for i in lst:
+        _sum+=i
+    return _sum 
+    
+s=get_sum([1,2,3,4,5])
+print(s)
+
+
+
