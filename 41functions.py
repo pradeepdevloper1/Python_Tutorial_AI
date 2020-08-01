982c1158a3f5e2bd1150c0da188f118e110b4e03
Pradeep Kumar <67544291+pradeepdevloper1@users.noreply.github.com>
2020-08-01 14:46:33 +0530

    Create 41functions.py
    function

---
+++ 41functions.py
@@ -0,0 +1,11 @@
+
+def print_name(name):
+    """
+    this function print name
+    """
+    print("Hello "+ str(name))
+
+print_name('Pradeep')  
+#Doc String 
+print("Doc string "+print_name.__doc__)
+ 
