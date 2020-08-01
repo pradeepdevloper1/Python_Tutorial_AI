998718d15248422c4a30e26d40bbc6d6b7436b14
Pradeep Kumar <67544291+pradeepdevloper1@users.noreply.github.com>
2020-08-01 16:34:51 +0530

    reduce function

--- 41functions.py
+++ 41functions.py
@@ -5,7 +5,9 @@ def print_name(name):
     """
     print("Hello "+ str(name))
 
-print_name('Pradeep')  
+print_name('Pradeep') 
+
+
 #Doc String 
 print("Doc string "+print_name.__doc__)
  
@@ -22,6 +24,8 @@ def get_sum(lst):
     
 s=get_sum([1,2,3,4,5])
 print(s)
+
+
 
 
 # lifetime of a variable
@@ -72,9 +76,54 @@ print(all(lst))
 lst=[2,3,4,5,6,7,8,9,False]
 print(all(lst))
 
+
+
 #dir
 numbers=[1,2,3,4,5]
 print(dir(numbers))
 
+
+
 #edivmod
 print(divmod(9,2))
+
+#filter function
+number_list=range(-10,10)
+print(list(number_list))
+
+def find_positive_number(number):
+    return number>0
+    
+pos_num_list=list(filter(find_positive_number,number_list))
+print(pos_num_list)
+
+
+
+#isinstance
+lst=[1,2,3,4,5,6,7]
+print(isinstance(lst,list))
+
+lst2=(1,2,3,4,5,6,7)
+print(isinstance(lst2,list))
+
+
+
+#map Function
+numbers=[1,2,3,4,5,6,7,8,9]
+
+def poweroftwo(num):
+    return num**2
+    
+squared =list(map(poweroftwo,numbers))  
+print(squared)
+
+
+#Reduce function
+def multiply(a,b):
+    return a*b
+lst=[1,2,3,4]
+from functools import reduce
+product=reduce(multiply,lst)
+print(product)
+
+    
